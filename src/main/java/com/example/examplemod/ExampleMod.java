5831c39b578e34b77a66fb320220c2e29f6d0547
Anthony Wittig <anthony.wittig@gmail.com>
2020-12-09 21:32:21 -0700

    item showing up

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -3,18 +3,28 @@ package com.example.examplemod;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.Item.Properties;
 
 import java.util.stream.Collectors;
 
@@ -22,6 +32,13 @@ import java.util.stream.Collectors;
 @Mod("examplemod")
 public class ExampleMod
 {
+	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "examplemod");
+	private static final Properties PIZZA_PROPS = new Properties()
+			.addToolType(ToolType.AXE, 3)
+			.group(ItemGroup.COMBAT)
+			.maxDamage(10);
+	private static final RegistryObject<Item> PIZZA_REGISTRY = ITEMS.register("pizza", () -> new Pizza(PIZZA_PROPS));
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -37,6 +54,8 @@ public class ExampleMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     private void setup(final FMLCommonSetupEvent event)
---
+++ src/main/java/com/example/examplemod/Pizza.java
@@ -0,0 +1,11 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+
+public class Pizza extends Item {
+
+	public Pizza(Properties properties) {
+		super(properties);
+		// TODO Auto-generated constructor stub
+	}
+}
